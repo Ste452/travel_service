73c8eb028fedb81b78ee147ed4afd9ee47c6c9fb
Steve <falecomstevevaz@gmail.com>
2026-02-20 10:35:55 -0300

    finish carousel image galery and start working accomodation search container

--- js/index.js
+++ js/index.js
@@ -4,16 +4,21 @@ const body = document.querySelector("body"),
       searchBtn = document.getElementById("search"),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
+      buttonActive = body.querySelector(".active");
 
-
-modeSwitch.addEventListener("click", () => {
+modeSwitch.addEventListener("click", function() {
     body.classList.toggle("dark");
 });
 
-toggle.addEventListener("click", () => {
+toggle.addEventListener("click", function() {
     sideBar.classList.toggle("close");
 });
 
-toggle.addEventListener("click", () => {
+toggle.addEventListener("click", function() {
     sideBar.classList.toggle("open");
-})
+});
+
+buttonActive.addEventListener("click", function() {
+    body.classList.toggle("selected");
+});
+
